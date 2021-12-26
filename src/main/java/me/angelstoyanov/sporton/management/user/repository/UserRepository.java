40ae29fa7f6eb694cb397a9bb2b3977b8c299c48
Angel Stoyanov <aststoyanov@gmail.com>
2021-12-26 21:42:11 +0200

    [Update/Add] Add UserResource and UserRepository to support basic REST CRUD operations using Azure Cosmos with the MongoDB API

--- src/main/java/me/angelstoyanov/sporton/management/user/repository/UserRepository.java
+++ src/main/java/me/angelstoyanov/sporton/management/user/repository/UserRepository.java
@@ -1,4 +1,44 @@
 package me.angelstoyanov.sporton.management.user.repository;
 
-public class UserRepository {
+
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import me.angelstoyanov.sporton.management.user.model.User;
+import org.bson.types.ObjectId;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.util.List;
+
+@Named("UserRepository")
+@ApplicationScoped
+public class UserRepository implements PanacheMongoRepository<User> {
+
+    public User findByEmail(String email) {
+        return find("email", email).firstResult();
+    }
+
+    public List<User> findByLocation(String location) {
+        return list("location", location);
+    }
+
+    public void addUser(User user) {
+        persist(user);
+    }
+
+    public void deleteUser(User user) {
+        delete(user);
+    }
+
+    public User updateUser(ObjectId id, User user) {
+        User userToUpdate = findById(id);
+        // Just testing update
+        //FIXME: NOT NULL SAFETY, DEPENDENT, ABSOLUTE DATA REPLACE
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setLastName(user.getLastName());
+        userToUpdate.setLocation(user.getLocation());
+        userToUpdate.setId(userToUpdate.getId());
+        persistOrUpdate(userToUpdate);
+        return userToUpdate;
+    }
 }
--- src/main/java/me/angelstoyanov/sporton/management/user/resource/UserResource.java
+++ src/main/java/me/angelstoyanov/sporton/management/user/resource/UserResource.java
@@ -1,4 +1,61 @@
 package me.angelstoyanov.sporton.management.user.resource;
 
+
+import me.angelstoyanov.sporton.management.user.model.User;
+import me.angelstoyanov.sporton.management.user.repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
+import java.util.List;
+
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
+    @Inject
+    protected UserRepository userRepository;
+
+    @POST
+    @ResponseStatus(201)
+    @Path("/user")
+    public User createUser(User user) {
+        userRepository.addUser(user);
+        return userRepository.findByEmail(user.getEmail());
+    }
+
+    @PATCH
+    @ResponseStatus(200)
+    @Path("/user/{id}")
+    public User updateUser(@PathParam("id") String id, User user) {
+        return userRepository.updateUser(new ObjectId(id), user);
+    }
+
+    @GET
+    @ResponseStatus(200)
+    @Path("/user/{id}")
+    public User getUser(@PathParam("id") String id) {
+        return userRepository.findById(new ObjectId(id));
+    }
+
+    @GET
+    @ResponseStatus(200)
+    @Path("/user")
+    public List<User> getUsers(@QueryParam("location") String location) {
+        if (location != null) {
+            return userRepository.findByLocation(location);
+        }
+        return new LinkedList<>();
+    }
+
+    @DELETE
+    @ResponseStatus(200)
+    @Path("/user/{id}")
+    public void deleteUser(@PathParam("id") String id) {
+        userRepository.deleteById(new ObjectId(id));
+    }
 }
